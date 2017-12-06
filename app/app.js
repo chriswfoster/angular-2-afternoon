5d2f00c12584cc946f0bcdd1975095d7e7bd6d0f
Chris Foster <chriswfoster@gmail.com>
2017-12-06 14:28:56 -0600

    step 3 complete

--- app/app.js
+++ app/app.js
@@ -3,7 +3,35 @@ angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider,
     .state('home',{
       url:'/',
       templateUrl: "app/about/aboutTmpl.html"
-    });
+    })
+    .state('packages', {
+      url: '/packages/:country',
+      templateUrl: "app/packages/packagesTmpl.html",
+      controller: "packagesCtrl"
+    })
+    .state('locations', {
+      url: '/locations',
+      templateUrl: "app/locations/locationsTmpl.html",
+      controller: "locationsCtrl"
+    })
+    .state('booked', {
+      url: '/booked',
+      templateUrl: "app/booked/bookedTmpl.html",
+      controller: "bookedCtrl"
+    })
+    .state('adventurers', {
+      url: '/adventurers',
+      templateUrl: "app/about/adventurers/adventurersTmpl.html",
+      parent: 'home'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: "app/contact/contactTmpl.html",
+      parent: 'home'
+    })
+
+
+
 
   $urlRouterProvider
     .otherwise('/');
